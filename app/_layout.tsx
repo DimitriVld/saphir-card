d9c84d1b3653858a1b3ec7bd3d9a5b78c27e0e7b
Dimitri Vildina <Dimitri.vildina@gmail.com>
2024-08-12 16:54:23 +0200

    feat: Create all screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+import CreateScreen from '@/screens/account/create';
+import ForgotPasswordScreen from '@/screens/account/forgotPassword';
+import GameScreen from '@/screens/game';
 import HomeScreen from '@/screens/home';
+import IntroScreen from '@/screens/intro';
+import LoginScreen from '@/screens/login';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,8 +16,28 @@ export default function RootLayout() {
     <NavigationContainer independent={true}>
       <Stack.Navigator>
         <Stack.Screen
+          name="Intro"
+          component={IntroScreen}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen
           name="Home"
           component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Game"
+          component={GameScreen}
+        />
+        <Stack.Screen
+          name="Create"
+          component={CreateScreen}
+        />
+        <Stack.Screen
+          name="ForgotPassword"
+          component={ForgotPasswordScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
